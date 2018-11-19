7ba8517bf26b7801c071380311c92687bf1d41c7
Mohammed Ersan <MOHAMMED.ERSAN@travelport.com>
2018-11-19 15:04:12 +0000

    - add new module for the network API

--- app/src/androidTest/java/space/ersan/movlan/data/source/MoviesRepositoryTest.kt
+++ app/src/androidTest/java/space/ersan/movlan/data/source/MoviesRepositoryTest.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.runner.AndroidJUnit4
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.After
@@ -18,7 +20,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import space.ersan.movlan.FakeMoviesApi
 import space.ersan.movlan.MoviesFactory
-import space.ersan.movlan.TestCoroutineDispatchers
 import space.ersan.movlan.app.MovlanApp
 import space.ersan.movlan.data.model.Genre
 import space.ersan.movlan.data.source.local.DefaultMoviesDbBoundaryCallbackFactory
@@ -26,6 +27,7 @@ import space.ersan.movlan.data.source.local.MoviesDb
 import space.ersan.movlan.data.source.local.MoviesLocalDataSource
 import space.ersan.movlan.data.source.remote.MoviesRemoteDataSource
 import space.ersan.movlan.mock
+import space.ersan.movlan.utils.AppCoroutineDispatchers
 import space.ersan.movlan.utils.LiveNetworkStatus
 
 @RunWith(AndroidJUnit4::class)
@@ -52,21 +54,22 @@ class MoviesRepositoryTest {
 
   @Before
   fun setUp() {
+    @Suppress("EXPERIMENTAL_API_USAGE")
+    val cors = AppCoroutineDispatchers(
+      IO = Dispatchers.Unconfined,
+      NETWORK = Dispatchers.Unconfined,
+      UI = Dispatchers.Unconfined
+    )
     val context = ApplicationProvider.getApplicationContext<MovlanApp>()
     database = Room.inMemoryDatabaseBuilder(context, MoviesDb::class.java)
       .build()
     val moviesDao = database.moviesDao()
-    val localDataSource = MoviesLocalDataSource(moviesDao)
-    val remoteDataSource = MoviesRemoteDataSource(fakeMoviesApi)
-    val networkStatus = LiveNetworkStatus()
+    val localDataSource = MoviesLocalDataSource(moviesDao, cors)
+    val remoteDataSource = MoviesRemoteDataSource(fakeMoviesApi, cors)
+
     val boundaryCallbackFactory = DefaultMoviesDbBoundaryCallbackFactory()
-    moviesRepository = DefaultMoviesRepository(
-      TestCoroutineDispatchers,
-      localDataSource,
-      remoteDataSource,
-      boundaryCallbackFactory,
-      networkStatus
-    )
+    moviesRepository =
+      DefaultMoviesRepository(localDataSource, remoteDataSource, boundaryCallbackFactory)
 
     lifecycleOwner = LifecycleOwner {
       LifecycleRegistry(mock()).apply {
@@ -82,27 +85,36 @@ class MoviesRepositoryTest {
 
   @Test
   fun getPopularMovies() {
-    val liveData = moviesRepository.getPopularMoviesPaginated()
-    liveData.observe(lifecycleOwner, Observer {
-      Assert.assertNotNull(it)
-      Assert.assertThat(it.size, `is`(equalTo(50)))
-      Assert.assertThat(it.last().title, `is`(equalTo(popularMovies.last().title)))
-    })
+    val networkStatus = LiveNetworkStatus()
+    runBlocking {
+      val liveData = moviesRepository.getPopularMoviesPaginated(networkStatus)
+      liveData.observe(lifecycleOwner, Observer {
+        Assert.assertNotNull(it)
+        Assert.assertThat(it.size, `is`(equalTo(50)))
+        Assert.assertThat(it.last().title, `is`(equalTo(popularMovies.last().title)))
+      })
+    }
   }
 
   @Test
   fun getMovieDetails() {
-    val movieLiveData = moviesRepository.getMovieDetails(1)
-    Assert.assertThat(movieLiveData.value, `is`(equalTo(popularMovies[0])))
+    runBlocking {
+      moviesRepository.getMovieDetails(1) {
+        Assert.assertThat(it, `is`(equalTo(popularMovies[0])))
+      }
+    }
   }
 
   @Test
   fun searchMovies() {
-    val liveData = moviesRepository.searchMovies("Hello")
-    liveData.observe(lifecycleOwner, Observer {
-      Assert.assertNotNull(it)
-      Assert.assertThat(it.size, `is`(equalTo(30)))
-      Assert.assertThat(it.last().title, `is`(equalTo(searchMovies.last().title)))
-    })
+    runBlocking {
+      val liveData = moviesRepository.searchMovies("Hello")
+      liveData.observe(lifecycleOwner, Observer {
+        val list = it.toList()
+        Assert.assertNotNull(list)
+        Assert.assertThat(list.size, `is`(equalTo(30)))
+        Assert.assertThat(list.last().title, `is`(equalTo(searchMovies.last().title)))
+      })
+    }
   }
 }
